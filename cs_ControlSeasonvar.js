ce7db1cd3daa91c7c891886ccc2977adc6788d97
MyName <shardofhapinees@gmail.com>
2021-08-24 07:36:24 +0300

    add seasonvar

---
+++ cs_ControlSeasonvar.js
@@ -0,0 +1,91 @@
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    switch(message.type) {
+        
+        case "getSpeed":
+          speed(message.val);
+          return true;
+            
+        break;
+        case "hotKeys":
+          hotKeys(message.val);
+          return true;
+        break;
+        default:
+        break;
+    }
+});
+setTimeout(()=>{
+    if(!document.getElementsByTagName("video")[0]){
+        return;
+    }
+    chrome.storage.sync.get({
+        videoSpeed: 1
+        
+    }, function(items) {
+        speed(items.videoSpeed);
+    });
+
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    var observer = new MutationObserver(function(mutations, observer) {
+        chrome.storage.sync.get({
+            videoSpeed: 1
+            
+        }, function(items) {
+            speed(items.videoSpeed);
+        });
+    
+    });
+
+    observer.observe(document.getElementsByTagName("video")[0], {
+        childList : true,
+        attributes: true
+    
+    });
+}, 1000);
+
+function hotKeys(value){
+    switch(value){
+      case "Speed1":
+        chrome.storage.sync.get({
+          HotKey1: 0
+          
+        }, function(items) {
+          speed(items.HotKey1);
+        });
+        break;
+      case "Speed2":
+        chrome.storage.sync.get({
+          HotKey2: 0
+          
+        }, function(items) {
+          speed(items.HotKey2);
+        });
+        break;
+      case "Speed3":
+        chrome.storage.sync.get({
+          HotKey3: 0
+          
+        }, function(items) {
+          speed(items.HotKey3);
+        });
+        break;
+      default: 
+        chrome.storage.sync.get({
+          HotKey1: 0
+          
+        }, function(items) {
+          speed(items.HotKey1);
+        });
+        break;
+    }
+}
+
+  function speed(speed){
+    chrome.storage.sync.set({
+      videoSpeed: speed
+    });
+    if(document.getElementsByTagName("video")[0]){
+        document.getElementsByTagName("video")[0].playbackRate=speed;
+    }    
+  }
